3e4764b4b7db32ba2f29927d74f4ab7f0d7eb7a3
Colton Robbins <coltonrobbins73@gmail.com>
2024-06-19 11:10:48 -0700

    refining cloudformation

--- cli_commands.bash
+++ cli_commands.bash
@@ -1,6 +1,9 @@
 # Validate the CloudFormation template
 aws cloudformation validate-template --template-body file://C:/Users/colto/Desktop/aws/data_pipeline/data_lake.yml
 
+# Make a quick graph of the CloudFormation template
+# For pub quality graphs, use aws CloudFormation Designer
+cfn-lint data_lake.yml -g
 
 # Create the CloudFormation stack
 aws cloudformation create-stack \
